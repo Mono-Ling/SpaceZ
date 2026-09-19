903f60e431b49d9c2d24040edd03223210b7839d
Mono-Ling <zouzongfeng070812@163.com>
2026-09-19 13:37:40 +0800

    新增编辑器模式下日志输出回调

---
+++ Assets/Scripts/Framework/PluginFramework/Plugin/NativeDebug.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public static class NativeDebug
+{
+#if UNITY_EDITOR
+    private const string PLUGIN_NAME = "SpaceZ_NativePlugins";
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    private delegate void DebugCallback(IntPtr msgPtr, int size);
+
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    private delegate void InjectDebugCallback(DebugCallback logPtr, DebugCallback warningPtr, DebugCallback errorPtr);
+    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+    private delegate void UnloadDebugCallback();
+
+    [AOT.MonoPInvokeCallback(typeof(DebugCallback))]
+    static void OnLogDebug(IntPtr msgPtr, int size)
+    => Debug.Log(Marshal.PtrToStringUTF8(msgPtr,size));
+
+    [AOT.MonoPInvokeCallback(typeof(DebugCallback))]
+    static void OnWarningDebug(IntPtr msgPtr, int size)
+    => Debug.LogWarning(Marshal.PtrToStringUTF8(msgPtr,size));
+
+    [AOT.MonoPInvokeCallback(typeof(DebugCallback))]
+    static void OnErrorDebug(IntPtr msgPtr, int size)
+    => Debug.LogError(Marshal.PtrToStringUTF8(msgPtr,size));
+    private static NativeLoader _nativeLoader;
+    private static InjectDebugCallback _injectCallback;
+    private static UnloadDebugCallback _unloadCallback;
+
+    private static DebugCallback _logCallback;
+    private static DebugCallback _warningCallback;
+    private static DebugCallback _errorCallback;
+    [RunTimeStart(-90)]
+    private static void Initialize()
+    {
+        if (_nativeLoader != null) return;
+        try
+        {
+            _nativeLoader = new NativeLoader(PLUGIN_NAME);
+            _nativeLoader.GetFunction("InjectDebugCallback", out _injectCallback);
+            _nativeLoader.GetFunction("UnloadDebugCallback",out _unloadCallback);
+            
+            _logCallback = OnLogDebug;
+            _warningCallback = OnWarningDebug;
+            _errorCallback = OnErrorDebug;
+            _injectCallback?.Invoke(_logCallback,_warningCallback,_errorCallback);
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"【NativeDebug】Native插件初始化失败：{ex}");
+            _injectCallback = null;
+        }
+    }
+    [RunTimeEnd(90)]
+    private static void Uninitialize()
+    {
+        try
+        {
+            _unloadCallback?.Invoke();
+        }
+        catch (Exception ex)
+        {
+            Debug.LogError($"【NativeDebug】卸载回调异常：{ex}");
+        }
+
+        if (_nativeLoader != null)
+        {
+            _nativeLoader.Dispose();
+            _nativeLoader = null;
+        }
+        _injectCallback = null;
+    }
+#endif
+}
--- SpaceZ_NativePlugins/UnityNativePlugins/Debug/NativeDebug.cpp
+++ SpaceZ_NativePlugins/UnityNativePlugins/Debug/NativeDebug.cpp
@@ -3,6 +3,12 @@
 
 static IUnityLog* unityLogPtr = nullptr;
 
+typedef void (*DebugCallback)(const char* message, int size);
+static DebugCallback logCallbackPtr = nullptr;
+static DebugCallback warningCallbackPtr = nullptr;
+static DebugCallback errorCallbackPtr = nullptr;
+
+#pragma region Unity日志注入
 extern "C" void UNITY_INTERFACE_EXPORT UNITY_INTERFACE_API
 UnityPluginLoad(IUnityInterfaces* interfacePtr)
 {
@@ -14,21 +20,49 @@ UnityPluginUnload()
 {
     unityLogPtr = nullptr;
 }
+#pragma endregion
+
+#pragma region Debug回调注入
+extern "C" void UNITY_INTERFACE_EXPORT
+InjectDebugCallback(DebugCallback logPtr, DebugCallback warningPtr, DebugCallback errorPtr)
+{
+    logCallbackPtr = logPtr;
+    warningCallbackPtr = warningPtr;
+    errorCallbackPtr = errorPtr;
+}
+extern "C" void UNITY_INTERFACE_EXPORT
+UnloadDebugCallback()
+{
+    logCallbackPtr = nullptr;
+    warningCallbackPtr = nullptr;
+    errorCallbackPtr = nullptr;
+}
+#pragma endregion
+
 namespace NativePlugins::Debug
 {
     void Log(const std::string& str)
     {
         if(unityLogPtr)
             UNITY_LOG(unityLogPtr, str.data());
+        else
+            if(logCallbackPtr)
+                logCallbackPtr(str.data(),str.size());
     }
     void LogWarning(const std::string& str)
     {
         if(unityLogPtr)
             UNITY_LOG_WARNING(unityLogPtr, str.data());
+        else
+            if(warningCallbackPtr)
+                warningCallbackPtr(str.data(),str.size());
     }
     void LogError(const std::string& str)
     {
         if(unityLogPtr)
             UNITY_LOG_ERROR(unityLogPtr, str.data());
+        else
+            if(errorCallbackPtr)
+                errorCallbackPtr(str.data(),str.size());
     }
 }
